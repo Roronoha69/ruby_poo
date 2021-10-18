fa2bd3280b6ebb0ce1efce81cc2dd65e61ad2662
Justin Mbodje <88967908+Roronoha69@users.noreply.github.com>
2021-10-18 20:51:05 +0200

    Add files via upload

---
+++ user.rb
@@ -0,0 +1,27 @@
+class User
+    attr_accessor :email, :age
+    @@all_users = []
+  
+    def initialize(email_to_save, age_to_save)
+      @email = email_to_save
+      @age = age_to_save.to_i
+      @@all_users.push(self)
+    end
+
+
+  def self.all
+    return @@all_users
+  end
+
+  def self.find_by_email(email)
+    @@all_users.each do |user|
+
+      if user.email == email
+        return user
+      end  
+    end
+  end
+
+
+  end
+
